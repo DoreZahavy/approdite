3eef3f7690eaeac23c7494898565450f047df9b8
Yona <dvsscheme@gmail.com>
2023-07-05 17:09:08 +0300

    added compose email

---
+++ apps/mail/cmps/ComposeEmail.js
@@ -0,0 +1,11 @@
+export default{
+    template:`
+        <section>
+            <form>
+                <input type="email" name="recipient">
+                <textarea rows="9" cols="30"></textarea>
+                <button>send</button>
+            </form>
+        </section>
+    `
+}
