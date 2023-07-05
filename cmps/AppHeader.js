7fb9aeec421d15ab175fece266bc63dc4cb1ef8c
Yona <dvsscheme@gmail.com>
2023-07-05 22:56:44 +0300

    styling

--- cmps/AppHeader.js
+++ cmps/AppHeader.js
@@ -1,7 +1,8 @@
 export default {
 	template: `
         <header class="app-header">
-            <h1>Approdite</h1>
+            <!-- <h1>Approdite</h1 -->
+             <img src="../assets/img/logo.svg">
             <nav>
                 <router-link to="/">Home</router-link> | 
                 <router-link to="/about">About</router-link> | 
