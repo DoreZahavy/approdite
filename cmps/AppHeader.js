b42c5919059b013cd7047470262405dcc90f5f24
Yona <dvsscheme@gmail.com>
2023-07-05 23:03:24 +0300

    small file directory bug fixes

--- cmps/AppHeader.js
+++ cmps/AppHeader.js
@@ -2,7 +2,7 @@ export default {
 	template: `
         <header class="app-header">
             <!-- <h1>Approdite</h1 -->
-             <img src="../assets/img/logo.svg">
+             <img src="assets/img/logo.svg">
             <nav>
                 <router-link to="/">Home</router-link> | 
                 <router-link to="/about">About</router-link> | 
