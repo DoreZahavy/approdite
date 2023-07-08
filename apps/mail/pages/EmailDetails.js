2429b290d534f63528854b605cdccb77dff5918e
Dore Zahavy <dorezahavy@gmail.com>
2023-07-08 21:22:54 +0300

    added user msg

--- apps/mail/pages/EmailDetails.js
+++ apps/mail/pages/EmailDetails.js
@@ -1,5 +1,8 @@
 import { emailService } from "../services/email.service.js"
 import { noteService } from "../../keep/services/note.service.js"
+import { showErrorMsg, showSuccessMsg } from "../../../services/event-bus.service.js"
+
+
 export default {
     // props: ['email'],
     template: `
@@ -65,7 +68,13 @@ export default {
                     backgroundColor: '#bababa'
                 }
             }
-            noteService.save(note).then(console.log('email saved as note'))
+            noteService.save(note).then(()=>{
+                showSuccessMsg('Email saved as note')
+                console.log('email saved as note')
+            })
+            .catch(err=>{
+                showErrorMsg('Cannot save as note')
+            })
         }
     },
     computed: {
