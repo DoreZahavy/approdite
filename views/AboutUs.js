17432d48a0ee22a608e3b73ff9c1bb880c26b125
Yona <dvsscheme@gmail.com>
2023-07-08 19:32:04 +0300

    more fixes

--- views/AboutUs.js
+++ views/AboutUs.js
@@ -7,7 +7,7 @@ export default {
                     <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, laborum! Eum voluptas veniam sequi, quibusdam omnis tenetur error amet corrupti doloremque, nostrum impedit porro sapiente laboriosam, quisquam dignissimos vero rem!</div>
                     <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, laborum! Eum voluptas veniam sequi, quibusdam omnis tenetur error amet corrupti doloremque, nostrum impedit porro sapiente laboriosam, quisquam dignissimos vero rem!</div>
                     <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, laborum! Eum voluptas veniam sequi, quibusdam omnis tenetur error amet corrupti doloremque, nostrum impedit porro sapiente laboriosam, quisquam dignissimos vero rem!</div>
-                    <img class="dore-img" src="../assets/img/dore.png" alt="" />
+                    <img class="dore-img" src="assets/img/dore.png" alt="" />
                     <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, laborum! Eum voluptas veniam sequi, quibusdam omnis tenetur error amet corrupti doloremque, nostrum impedit porro sapiente laboriosam, quisquam dignissimos vero rem!</div>
                     <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, laborum! Eum voluptas veniam sequi, quibusdam omnis tenetur error amet corrupti doloremque, nostrum impedit porro sapiente laboriosam, quisquam dignissimos vero rem!</div>
                     <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, laborum! Eum voluptas veniam sequi, quibusdam omnis tenetur error amet corrupti doloremque, nostrum impedit porro sapiente laboriosam, quisquam dignissimos vero rem!</div>
@@ -27,7 +27,7 @@ export default {
                 </section>
                 <section class="right-column">
                     <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, laborum! Eum voluptas veniam sequi, quibusdam omnis tenetur error amet corrupti doloremque, nostrum impedit porro sapiente laboriosam, quisquam dignissimos vero rem!</div>
-                    <img class="yona-img" src="../assets/img/yona.png" alt="" />
+                    <img class="yona-img" src="assets/img/yona.png" alt="" />
                     <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, laborum! Eum voluptas veniam sequi, quibusdam omnis tenetur error amet corrupti doloremque, nostrum impedit porro sapiente laboriosam, quisquam dignissimos vero rem!</div>
                     <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, laborum! Eum voluptas veniam sequi, quibusdam omnis tenetur error amet corrupti doloremque, nostrum impedit porro sapiente laboriosam, quisquam dignissimos vero rem!</div>
                     <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, laborum! Eum voluptas veniam sequi, quibusdam omnis tenetur error amet corrupti doloremque, nostrum impedit porro sapiente laboriosam, quisquam dignissimos vero rem!</div>
